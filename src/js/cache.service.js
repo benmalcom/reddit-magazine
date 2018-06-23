d75b66a1a7854600bfd1eb80d47123fedafaea85
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-06-23 09:37:12 +0100

    improved commenting on CacheService class

--- src/js/cache.service.js
+++ src/js/cache.service.js
@@ -1,12 +1,17 @@
+// The Cache class, using localStorage
+
+// Creates a CacheService class with optional key param
 function CacheService(key = null) {
 	this.key = key;
 	this.storage = localStorage;
 }
 
+// Set a new item key
 CacheService.prototype.setItemKey = function (key) {
 	this.key = key;
 };
 
+// Get an item by key
 CacheService.prototype.getItem = function () {
 	let item = this.storage.getItem(this.key);
 	try{
@@ -18,20 +23,24 @@ CacheService.prototype.getItem = function () {
 	return null;
 };
 
+// Store a new item with expiration(default is 5minutes according to project spec)
 CacheService.prototype.store = function (items, duration = 300000) {
 	const obj = {expiration: Date.now() + duration, items};
 	return this.storage.setItem(this.key, JSON.stringify(obj));
 };
 
+// Check if item is expired
 CacheService.prototype.expired = function () {
 	const item = this.getItem();
     return Date.now() > item.expiration;
 };
 
+// Remove the current item
 CacheService.prototype.removeItem = function () {
 	this.storage.removeItem(this.key);
 };
 
+// Remove every expired item
 CacheService.prototype.removeExpired = function() {
 	console.log('removing expired');
 	const keys = Object.keys(localStorage);
